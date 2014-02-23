93fe26b779de016fb498f4192b6d700edae2f83c
Frank Zhao <frank@frankzhao.net>
2014-02-23 20:52:20 +1100

    Initial Commit

---
+++ ImageSegmentation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImageSegmentation
+//
+//  Created by Frank Zhao on 23/02/2014.
+//  Copyright (c) 2014 Frank Zhao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
