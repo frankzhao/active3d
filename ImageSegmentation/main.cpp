b6e44b2c1a91697c3a3284237cf41588133fc399
Frank Zhao <frank.zhao@anu.edu.au>
2014-04-07 14:37:55 +1000

    about to add vertex rendering

--- ImageSegmentation/main.cpp
+++ ImageSegmentation/main.cpp
@@ -273,12 +273,15 @@ void display (void){
     
     glClear( GL_COLOR_BUFFER_BIT);
     glColor3f(0.0, 1.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(2.0, 4.0, 0.0);
-    glVertex3f(8.0, 4.0, 0.0);
-    glVertex3f(8.0, 6.0, 0.0);
-    glVertex3f(2.0, 6.0, 0.0);
-    glEnd();
+
+    
+//    glBegin(GL_POLYGON);
+//    glVertex3f(2.0, 4.0, 0.0);
+//    glVertex3f(8.0, 4.0, 0.0);
+//    glVertex3f(8.0, 6.0, 0.0);
+//    glVertex3f(2.0, 6.0, 0.0);
+//    glEnd();
+    
     glFlush();
     
 //    glClearColor (0.0, 0.0, 0.0, 1.0);
@@ -292,13 +295,11 @@ void display (void){
 
 //next we will create our window and display the "display" void:
 int renderGL (int argc, char **argv){
-    printf("hello world\n");
-    glutInit(&argc, argv);
     glutInitDisplayMode ( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     
     glutInitWindowPosition(100,100);
     glutInitWindowSize(300,300);
-    glutCreateWindow ("square");
+    glutCreateWindow ("GLUT");
     
     glClearColor(0.0, 0.0, 0.0, 0.0);         // black background
     glMatrixMode(GL_PROJECTION);              // setup viewing projection
