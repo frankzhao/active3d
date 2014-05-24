4c596e5ed0596ce8bf0d3f15596e59ba2a839803
Frank Zhao <frank.zhao@anu.edu.au>
2014-05-24 23:17:36 +1000

    Compile successful

--- active3d/utility.h
+++ active3d/utility.h
@@ -13,21 +13,21 @@
 
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
+//using namespace cv;
 
-int overlayImage(Mat src, Mat overlay, Mat dest);
+int overlayImage(cv::Mat src, cv::Mat overlay, cv::Mat dest);
 
 // apply a mask with unmasked areas in alpha=0
-int applyMask(Mat src, Mat mask, Mat dest);
+int applyMask(cv::Mat src, cv::Mat mask, cv::Mat dest);
 
 // Count how many of the 8 neighbouring pixels
 // are of a particular value (8-bit, 1-channel)
-int countNeighbours(Mat m, int v, int row, int col);
+int countNeighbours(cv::Mat m, int v, int row, int col);
 
 // Generate depth map with specified iterations
-float depthMap(Mat mask, Mat dest, const string &winname, int iterations);
+float depthMap(cv::Mat mask, cv::Mat dest, const cv::string &winname, int iterations);
 
 // Print float matricies nicely
-void printMatrix(Mat m);
+void printMatrix(cv::Mat m);
 
 #endif
